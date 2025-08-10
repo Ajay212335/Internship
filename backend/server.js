2482d2e9ffccfa7fd47d244aef66162b5058ffae
Ajay212335 <ajaiks2005@gmail.com>
2025-08-11 03:45:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const fetch = require('node-fetch'); // npm i node-fetch@2
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
 
 ///////////////////// CONFIG - from environment variables /////////////////////
 
